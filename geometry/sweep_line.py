a87cc84fdba55fd73230e9c91db36bf8b188cee2
codyjackson <crystalattice@gmail.com>
2019-10-11 11:01:01 -0500

    Modified sweep line example with comments

--- geometry/sweep_line.py
+++ geometry/sweep_line.py
@@ -8,55 +8,53 @@ class Building:
         self.finished = False
 
     def __lt__(self, other):
-        # Reverse order by height, so that when we store buildings in
-        # a heap, the first building is the highest.
+        """Python uses min-heaps so, if we reverse order by height, when we store building_coordinates in a heap, the
+        first building is the highest."""
         return other.height < self.height
 
 
-# An event represents the buildings that start and end at a particular
+Structure = namedtuple('Structure', 'start end')  # Represents the buildings that start and end at a particular
 # x-coordinate.
-Event = namedtuple('Event', 'start end')
 
 
-def skyline(buildings):
-    """Given an iterable of buildings represented as triples (left, height, right), generate the co-ordinates of
-    the skyline.
-    """
-    # Map from x-coordinate to event.
-    events = defaultdict(lambda: Event(start=[], end=[]))
-    for left, height, right in buildings:
-        b = Building(height)
-        events[left].start.append(b)
-        events[right].end.append(b)
+def skyline(building_coordinates):
+    """Given an iterable of building_coordinates represented as triples (left, height, right), generate the coordinates
+    of the skyline."""
+    structures = defaultdict(lambda: Structure(start=[], end=[]))  # Map from x-coordinates to structure. defaultdict
+    # ensures that no errors are generated for keys that don't yet exist; a new Structure will simply be added to the
+    # dictionary.
+    for left, height, right in building_coordinates:
+        building = Building(height)
+        structures[left].start.append(building)  # The left x-coordinate is added to the start entry for the building;
+        # if a new building has the same x-coordinate as the previous, the tallest height is used
+        structures[right].end.append(building)  # The right x-coordinate is added to the end entry for the building
 
-    standing = []  # Heap of buildings currently standing.
-    last_h = 0  # Last emitted skyline height.
+    placed = []  # Heap of building_coordinates currently placed.
+    last_height = 0  # Last generated skyline height.
 
-    # Process events in order by x-coordinate.
-    for x, event in sorted(events.items()):
-        # Update buildings currently standing.
-        for b in event.start:
-            heappush(standing, b)
-        for b in event.end:
-            b.finished = True
+    for x, structure in sorted(structures.items()):  # Process structures in order by x-coordinate
+        for building in structure.start:  # Update building_coordinates currently placed
+            heappush(placed, building)  # Add building to heap
+        for building in structure.end:  # If building not overlapped, then it is finished
+            building.finished = True
 
-        # Pop any finished buildings from the top of the heap.
-        while standing and standing[0].finished:
-            heappop(standing)
+        while placed and placed[0].finished:  # Pop any finished building_coordinates from the top of the heap.
+            heappop(placed)
 
-        # Top of heap (if any) is the highest standing building, so
-        # its height is the current height of the skyline.
-        h = standing[0].height if standing else 0
+        if placed:  # Top of heap is the highest placed building, so its height is the current height of the skyline
+            skyline_height = placed[0].height
+        else:
+            skyline_height = 0
 
-        # Yield co-ordinates if the skyline height has changed.
-        if h != last_h:
-            yield x, h
-            last_h = h
+        if skyline_height != last_height:  # Yield co-ordinates if the skyline height has changed.
+            yield x, skyline_height
+            last_height = skyline_height
 
 
 if __name__ == "__main__":
-    print(list(skyline([(1, 9, 3), (1, 11, 5), (2, 6, 7), (3, 13, 9), (12, 7, 16), (14, 3, 25), (19, 18, 22),
-                        (23, 13, 29), (24, 4, 28)])))
-    print(list(skyline([(1, 3, 2), (1, 3, 2)])))
-    print(list(skyline([(1, 11, 5), (2, 6, 7), (3, 13, 9), (12, 7, 16), (14, 3, 25), (19, 18, 22), (23, 13, 29),
-                        (24, 4, 28)])))
+    print(f"Directly overlapping buildings: {list(skyline([(1, 3, 2), (1, 3, 2)]))}")
+    print(f"Offset overlapping buildings: {list(skyline([(1, 11, 7), (2, 9, 13), (3, 13, 15)]))}")
+    print(f"""Normal skyline: {list(skyline([(1, 9, 3), (1, 11, 5), (2, 6, 7), (3, 13, 9), (12, 7, 16), (14, 3, 25), 
+                                             (19, 18, 22), (23, 13, 29), (24, 4, 28)]))}""")
+
+
